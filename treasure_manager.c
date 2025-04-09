9e50939918256fc38b1bb7ece1eff45a1bce58de
Ghimboasa Casian <casian@Ghimboasas-MacBook-Pro.local>
2025-04-09 17:06:48 +0300

    modificare

--- treasure_manager.c
+++ treasure_manager.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-// Defineing a structure to hold treasure details
+
 typedef struct {
-    int TreasureID;
+    char TreasureID[50];
     char UserName[50];
     float Latitude;
     float Longitude;
@@ -11,142 +11,60 @@ typedef struct {
     int Value;
 }Treasure;
 
-void add()
+void add_treasure(const char *hunt_id)
 {
-    FILE *f;
-    char treasureName[100];
-    // Reading the file name from the user
-    printf("Enter the file name: ");
-    if((scanf("%s",treasureName))!=1)
-    {
-        printf("Error reading file name\n");
-        exit(-1);
-    }
-    // Open the file in append mode
-    // If the file does not exist, it will be created
-    if((f=fopen(treasureName,"w+"))==NULL)
-    {
-        printf("Error opening file\n");
-        exit(-1);
-    }
-    Treasure t;
-    // Reading treasure details from the user
-    // Write the treasure details to the file
-    printf("Enter Treasure ID: ");
-    if((scanf("%d", &t.TreasureID))!=1)
-    {
-        printf("Error reading Treasure ID\n");
-        exit(-1);
-    }
-    printf("Enter User Name: ");
-    if((scanf("%s", t.UserName))!=1)
-    {
-        printf("Error reading User Name\n");
-        exit(-1);
-    }
-    printf("Enter Latitude: ");
-    if((scanf("%f", &t.Latitude))!=1)
-    {
-        printf("Error reading Latitude\n");
-        exit(-1);
-    }
-    printf("Enter Longitude: ");
-    if((scanf("%f", &t.Longitude))!=1)
-    {
-        printf("Error reading Longitude\n");
-        exit(-1);
-    }
-    printf("Enter Clue Text: ");
-    getchar(); // Clear the newline character from the buffer
-    // Reading the clue text, even with spaces
-    if((fgets(t.ClueText, sizeof(t.ClueText)-1 , stdin))==NULL)
-    {
-        printf("Error reading Clue Text\n");
-        exit(-1);
-    }
-    // Remove the newline character from the clue text
-    size_t len = strlen(t.ClueText);
-    if (len > 0 && t.ClueText[len - 1] == '\n') {
-        t.ClueText[len - 1] = '\0';
-    }
-    printf("Enter Value: ");
-    if((scanf("%d", &t.Value))!=1)
-    {
-        printf("Error reading Value\n");
-        exit(-1);
-    }
-    // Write the treasure details to the file
-    if((fputs("Treasure ID: ", f))==EOF)
-    {
-        printf("Error writing to file\n");
-        exit(-1);
-    }
-    if((fprintf(f, "%d\n", t.TreasureID))<0)
-    {
-        printf("Error writing to file\n");
-        exit(-1);
-    }
-    if((fputs("User Name: ", f))==EOF)
-    {
-        printf("Error writing to file\n");
-        exit(-1);
-    }
-    if((fprintf(f, "%s\n", t.UserName))<0)
-    {
-        printf("Error writing to file\n");
-        exit(-1);
-    }
-    if((fputs("Latitude: ", f))==EOF)
-    {
-        printf("Error writing to file\n");
-        exit(-1);
-    }
-    if((fprintf(f, "%f\n", t.Latitude))<0)
-    {
-        printf("Error writing to file\n");
-        exit(-1);
-    }
-    if((fputs("Longitude: ", f))==EOF)
-    {
-        printf("Error writing to file\n");
-        exit(-1);
-    }
-    if((fprintf(f, "%f\n", t.Longitude))<0)
-    {
-        printf("Error writing to file\n");
-        exit(-1);
-    }
-    if((fputs("Clue Text: ", f))==EOF)
-    {
-        printf("Error writing to file\n");
-        exit(-1);
-    }
-    if((fprintf(f, "%s\n", t.ClueText))<0)
-    {
-        printf("Error writing to file\n");
-        exit(-1);
-    }
-    if((fputs("Value: ", f))==EOF)
-    {
-        printf("Error writing to file\n");
-        exit(-1);
-    }
-    if((fprintf(f, "%d\n", t.Value))<0)
-    {
-        printf("Error writing to file\n");
-        exit(-1);
-    }
-    // Close the file
-    if(fclose(f)!=0)
-    {
-        printf("Error closing file\n");
-        exit(-1);
-    }
-    printf("Treasure details added successfully\n");
+    
+}
+void list_treasures(const char *hunt_id)
+{
+
+}
+void view_treasure(const char *hunt_id, char *treasure_id)
+{
+
+}
+void remove_treasure(const char *hunt_id, char *treasure_id)
+{
+
+}
+void remove_hunt(const char *hunt_id)
+{
+
+}
+void log_operation(const char *hunt_id, const char *message)
+{
+
+}
+void create_symlink(const char *hunt_id)
+{
+
 }
 
-int main(void)
+
+int main(int argc, char **argv)
 {
-    add();
+    if (argc < 3) {
+        fprintf(stderr, "Usage:\n"
+                        "--add <hunt_id>\n"
+                        "--list <hunt_id>\n"
+                        "--view <hunt_id><treasure_id>\n"
+                        "--remove_treasure <hunt_id> <treasure_id>\n"
+                        "--remove_hunt <hunt_id>\n");
+        exit(-1);
+    }
+    if (strcmp(argv[1], "--add") == 0) {
+        add_treasure(argv[2]);
+    } else if (strcmp(argv[1], "--list") == 0) {
+        list_treasures(argv[2]);
+    } else if (strcmp(argv[1], "--view") == 0) {
+        view_treasure(argv[2], argv[3]);
+    } else if (strcmp(argv[1], "--remove_treasure") == 0) {
+        remove_treasure(argv[2], argv[3]);
+    } else if (strcmp(argv[1], "--remove_hunt") == 0) {
+        remove_hunt(argv[2]);
+    } else {
+        fprintf(stderr, "Invalid command\n");
+        exit(-1);
+    }
     return 0;
 }
