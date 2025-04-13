74e59b1831024ed65986e501997a4b0c427cc735
Ghimboasa Casian <casian@192.168.1.134>
2025-04-13 18:19:38 +0300

    Adaugare symlink

--- treasure_manager.c
+++ treasure_manager.c
@@ -43,6 +43,25 @@ void log_operation(const char *hunt_id, const char *message)
     printf("Operation logged successfully!\n");
 }
 
+void create_symlink(const char *hunt_id)
+{
+    char log_path[50];
+    char link[50];
+    snprintf(log_path, sizeof(log_path), "%s/logged_hunt.txt", hunt_id);
+    snprintf(link, sizeof(link), "logged_hunt-%s", hunt_id);
+    // Remove if exists
+    unlink(link);
+    if (symlink(log_path, link) == -1) 
+    {
+        printf("Error creating symlink\n");
+        exit(-1);
+    }
+    else
+    {
+        printf("Symlink created: %s -> %s\n", link, log_path);
+    }
+}
+
 void add_treasure(const char *hunt_id)
 {
     struct stat st = {0};
@@ -102,7 +121,6 @@ void add_treasure(const char *hunt_id)
     {
         t.ClueText[len - 1] = '\0';
     }
-
     printf("Enter Value: ");
     if ((scanf("%d", &t.Value)) != 1)
     {
@@ -118,9 +136,11 @@ void add_treasure(const char *hunt_id)
     }
     printf("Treasure added successfully!\n");
     // Log the operation
-    char log_msg[256];
-    sprintf(log_msg, "Added treasure ID %s by user %s\n", t.TreasureID, t.UserName);
-    log_operation(hunt_id, log_msg);
+    char msg[256];
+    sprintf(msg, "Added treasure ID %s by user %s\n", t.TreasureID, t.UserName);
+    log_operation(hunt_id, msg);
+    // Create a symlink to the log file
+    create_symlink(hunt_id);
     if(close(f)<0)
     {
         printf("Error closing the file\n");
@@ -168,9 +188,9 @@ void list_treasures(const char *hunt_id)
         exit(-1);
     }
     // Log the operation
-    char log_msg[256];
-    sprintf(log_msg, "List treasures in Hunt: %s\n", hunt_id);
-    log_operation(hunt_id, log_msg);
+    char msg[256];
+    sprintf(msg, "List treasures in Hunt: %s\n", hunt_id);
+    log_operation(hunt_id, msg);
     if(close(f)<0)
     {
         printf("Error closing the file\n");
@@ -185,7 +205,13 @@ void view_treasure(const char *hunt_id, char *treasure_id)
     char buffer[100]; 
     strcpy(buffer, hunt_id);
     strcat(buffer, "/treasure.dat");
-
+    struct stat info;
+    // Check if the file exists
+    if (stat(buffer, &info) != 0 || !S_ISREG(info.st_mode)) 
+    {
+        printf("File does not exist\n");
+        exit(-1);
+    }
     f = open(buffer, O_RDONLY);  // Opening the file for reading and checking if it exists
     if (f < 0) 
     {
@@ -223,9 +249,9 @@ void view_treasure(const char *hunt_id, char *treasure_id)
         exit(-1);
     }
     // Log the operation
-    char log_msg[256];
-    sprintf(log_msg, "View treasure ID %s by user %s\n", treasure_id, username);
-    log_operation(hunt_id, log_msg);
+    char msg[256];
+    sprintf(msg, "View treasure ID %s by user %s\n", treasure_id, username);
+    log_operation(hunt_id, msg);
     if(close(f)<0)
     {
         printf("Error closing the file\n");
@@ -240,6 +266,13 @@ void remove_treasure(const char *hunt_id, char *treasure_id)
     strcpy(buffer, hunt_id);
     strcat(buffer, "/treasure.dat");
     char username[50];
+    // Check if the file exists
+    struct stat info;
+    if (stat(buffer, &info) != 0 || !S_ISREG(info.st_mode)) 
+    {
+        printf("File does not exist\n");
+        exit(-1);
+    }
     // Opening the file for reading and checking if it exists
     f = open(buffer, O_RDWR);
     if (f == -1) 
@@ -270,18 +303,21 @@ void remove_treasure(const char *hunt_id, char *treasure_id)
         }
         write(temp_fd, &t, sizeof(Treasure));
     }
-    if (!treasure_found) {
+    if (!treasure_found) 
+    {
         printf("Treasure with ID %s not found.\n", treasure_id);
-    } else {
+    } 
+    else 
+    {
         printf("Treasure with ID %s removed successfully.\n", treasure_id);
         // Replace the old file with the new file
         unlink(buffer);
         rename(buffer2, buffer);
     }
     // Log the operation
-    char log_msg[256];
-    sprintf(log_msg, "Deleted treasure ID %s by user %s\n", treasure_id, username);
-    log_operation(hunt_id, log_msg);
+    char msg[256];
+    sprintf(msg, "Deleted treasure ID %s by user %s\n", treasure_id, username);
+    log_operation(hunt_id, msg);
     if(close(f)<0)
     {
         printf("Error closing the file\n");
@@ -294,7 +330,6 @@ void remove_treasure(const char *hunt_id, char *treasure_id)
     }  // Close the temporary file
     // Remove the temporary file
 }
-
 
 void remove_hunt(const char *hunt_id)
 {
@@ -327,15 +362,10 @@ void remove_hunt(const char *hunt_id)
     printf("Hunt '%s' removed.\n", hunt_id);
 }
 
-void create_symlink(const char *hunt_id)
-{
-    
-}
-
-
 int main(int argc, char **argv)
 {
-    if (argc < 3) {
+    if (argc < 3)
+    {
         fprintf(stderr, "Usage:\n"
                         "--add <hunt_id>\n"
                         "--list <hunt_id>\n"
@@ -344,17 +374,28 @@ int main(int argc, char **argv)
                         "--remove_hunt <hunt_id>\n");
         exit(-1);
     }
-    if (strcmp(argv[1], "--add") == 0) {
+    if (strcmp(argv[1], "--add") == 0) 
+    {
         add_treasure(argv[2]);
-    } else if (strcmp(argv[1], "--list") == 0) {
+    } 
+    else if (strcmp(argv[1], "--list") == 0) 
+    {
         list_treasures(argv[2]);
-    } else if (strcmp(argv[1], "--view") == 0) {
+    } 
+    else if (strcmp(argv[1], "--view") == 0) 
+    {
         view_treasure(argv[2], argv[3]);
-    } else if (strcmp(argv[1], "--remove_treasure") == 0) {
+    } 
+    else if (strcmp(argv[1], "--remove_treasure") == 0) 
+    {
         remove_treasure(argv[2], argv[3]);
-    } else if (strcmp(argv[1], "--remove_hunt") == 0) {
+    } 
+    else if (strcmp(argv[1], "--remove_hunt") == 0) 
+    {
         remove_hunt(argv[2]);
-    } else {
+    } 
+    else 
+    {
         fprintf(stderr, "Invalid command\n");
         exit(-1);
     }
